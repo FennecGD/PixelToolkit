4ba456886f3221c47866c94d8d0d54b3891a62bb
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2023-09-02 01:56:09 +0200

    corrected todo

--- modules/lib/utils.py
+++ modules/lib/utils.py
@@ -1,7 +1,7 @@
 # Stuff that will be shared by more than one lib
 import pyperclip
 
-# TODO add log function in utils.py 
+# TODO add log function
 
 def copy_to_clipboard(string_to_copy):
     pyperclip.copy(string_to_copy)
