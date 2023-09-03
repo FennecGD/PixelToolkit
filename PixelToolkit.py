bbebc728a32af2d5afa0795bedd4e73285c51b02
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2023-09-03 23:11:02 +0200

    Made port scanner script and added subparser allowing to use it

--- PixelToolkit.py
+++ PixelToolkit.py
@@ -11,9 +11,12 @@ if __name__ == "__main__":
 
     pass_gen = subparsers.add_parser("pass-gen", help="Password Generator")
     pass_gen.add_argument("--length", "-l", type=int, help="Length of the password", default=10)
+    
+    port_scan = subparsers.add_parser("port-scan", help="Port Scanner")
+    port_scan.add_argument("--host", type=str, help="Host to scan", default="127.0.0.1")
+    port_scan.add_argument("--ports", "-p", type=str, dest="port_range", default="1-65535", help="Port range to scan. Default is from 1 to 65535 (every port)")
 
     args = parser.parse_args()
-
     if len(sys.argv) == 1:  # If no arguments passed -> GUI
         from lib.gui import main_window_generator
         main_window_generator()
@@ -21,3 +24,16 @@ if __name__ == "__main__":
         if args.subcommand == "pass-gen":
             generator = PasswordGenerator()
             print("Password: " + generator.gen(args.length))
+
+        elif args.subcommand == "port-scan":
+            print(args)
+            from lib.port_scanner import manage_port_scan
+            host, port_range = args.host, args.port_range
+
+            start_port, end_port = port_range.split("-")
+            start_port, end_port = int(start_port), int(end_port)
+
+            ports = [port for port in range(start_port, end_port)]
+            manage_port_scan(host, ports)
+
+
---
+++ lib/port_scanner.py
@@ -0,0 +1,50 @@
+import queue
+import socket
+import argparse
+from threading import Thread, Lock
+from queue import Queue
+
+N_THREADS = 150
+
+queue = Queue()
+print_lock = Lock()
+
+def scan_port(host, port):
+    try:
+        sock = socket.socket()
+        sock.connect((host, port))
+    except:
+        pass
+    else:
+        with print_lock:
+            print(f"{host:15}:{port:5} is open")
+    finally:
+        sock.close()
+
+
+def scan_thread(host):
+    global queue
+    while True:
+        port_number = queue.get()
+        scan_port(host, port_number)
+        queue.task_done()
+
+
+def manage_port_scan(host, ports):
+    global queue
+    print("test")
+    for thread in range(N_THREADS):
+        thread = Thread(target=scan_thread, args=(host,))
+        # if daemon is True that thread will end when the main threadends
+        thread.daemon = True
+        thread.start()
+
+    for port in ports:
+        # put each port to queue to start scan
+        queue.put(port)
+
+    queue.join()
+
+
+
+
